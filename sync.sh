873d5ce80f94cb8a50b6d2a4c2110728698145aa
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-04-22 18:21:14 -0700

    Update sync script.

--- sync.sh
+++ sync.sh
@@ -1,1 +1,17 @@
-s3cmd sync -P --no-mime-magic -M site/ s3://dev.cosmo4d.org
+
+
+# Requires s3cmd-1.6.1
+# Need to set up the S3 credential
+
+if [ "x$1" == --release ]; then
+target=s3://cosmo4d.org
+else
+target=s3://dev.cosmo4d.org
+fi
+
+# --no-mime-magic is important because python-magic
+# is likely broken.
+#
+# e.g. https://github.com/s3tools/s3cmd/issues/198
+
+s3cmd sync -P --no-mime-magic -M site/ $target
